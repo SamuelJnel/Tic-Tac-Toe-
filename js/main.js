45e5c506d67404fa8c0d86fde700b231124f00b1
Jonelle Samuel <jonellesamuel@Jonelles-MacBook-Air.local>
2021-07-26 16:35:59 -0400

    completed game

---
+++ js/main.js
@@ -0,0 +1,84 @@
+let turn = 1;
+let winner = null;
+let displayText;
+const blankSpace = [null, null, null, null, null, null, null, null, null];
+
+const winCombinations = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+let gameBoard = document.querySelectorAll(".game-container");
+gameBoard = blankSpace;
+
+let playSquares = document.querySelectorAll(".square");
+
+let reSetBtn = document.querySelector(".reset-btn");
+
+let displayResults = document.querySelector(".display-results");
+
+playSquares.forEach((el) => el.addEventListener("click", gameBoardFunct));
+
+reSetBtn.addEventListener("click", reSet);
+
+function switchTurn(el) {
+  turn == 1 ? (turn = -1) : (turn = 1);
+  // if turn 1 is true switch to trun -1 if false switch to turn 1.
+}
+
+function gameBoardFunct(evt) {
+  displayText = evt.target;
+  let boxClicked = evt.target.id;
+  if (evt.target.innerText != "") return;
+  if (winner) return;
+  if (turn == 1) {
+    displayText.style.color = "#346751";
+    displayText.innerHTML = "X";
+  } else {
+    turn = -1;
+    displayText.style.color = "#DA0037";
+    displayText.innerHTML = "O";
+  }
+
+  blankSpace[boxClicked] = evt.target.innerText;
+  findWinner();
+  switchTurn();
+  console.log(gameBoard);
+}
+
+function findWinner(el) {
+  winCombinations.forEach(function (el, idx) {
+    if (
+      blankSpace[el[0]] &&
+      blankSpace[el[0]] === blankSpace[el[1]] &&
+      blankSpace[el[0]] === blankSpace[el[2]]
+    ) {
+      winner = blankSpace[el[0]];
+      displayResults.innerHTML = `${winner} has won!`;
+    } else if (
+      !blankSpace.includes(null) &&
+      blankSpace[el[0]] != blankSpace[el[1]] &&
+      blankSpace[el[0]] != blankSpace[el[2]]
+    ) {
+      displayResults.innerHTML = ` It's a tie!`;
+    }
+  });
+}
+
+function reSet() {
+  turn = 1;
+  gameBoard.fill(null);
+  console.log(gameBoard);
+  console.log(blankSpace);
+  playSquares.forEach((el) => (el.innerHTML = " "));
+  displayResults.innerHTML = `Who will win?`;
+  winner = null;
+}
+
+//(!blankSpace.includes(null) && turn == 1)
